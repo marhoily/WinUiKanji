a3d79b62417756d31db3fb949e1e37e314c2aadb
Ilya Margolin <imargolin@luware.com>
2021-11-07 22:42:18 +0300

    +Study sets

--- WinUiKanji/WinUiKanji/MainViewModel.cs
+++ WinUiKanji/WinUiKanji/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.IO;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using Windows.Media.Core;
 using Windows.Media.Playback;
 using Windows.Media.SpeechSynthesis;
+using CommunityToolkit.WinUI.UI.Controls.TextToolbarSymbols;
 using CsvHelper;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
@@ -16,48 +18,80 @@ namespace WinUiKanji
     public partial class MainViewModel : ObservableObject
     {
         private readonly Random _rnd = new();
-        private readonly SpeechSynthesizer _synthesizer = new()
-        {
-            Voice = SpeechSynthesizer.AllVoices.First(v => v.Language == "ja-JP")
-        };
+        private readonly SpeechSynthesizer _synthesizer = new();
+
+        private const bool ReadAnswerEnabled = true;
+        private bool _answerIsRead = false;
+
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(CurrentCard))]
+        [AlsoNotifyChangeFor(nameof(CurrentIndexStr))]
+        private int _currentTermIndex;
+
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(CurrentCard))]
+        [AlsoNotifyChangeFor(nameof(SourceSetLength))]
+        private List<Card> _sourceSet;
+        private Card[] _originalSet;
+
+        public Card CurrentCard => _sourceSet[CurrentTermIndex];
+        public string CurrentIndexStr => (CurrentTermIndex + 1).ToString();
+        public string SourceSetLength => SourceSet.Count.ToString();
 
         public MainViewModel()
         {
             BackgroundMediaPlayer.Current.AutoPlay = true;
-            using var reader = new StreamReader(@"C:\git\Kanji\WordsToStudy4.csv");
+            using var reader = new StreamReader(@"C:\git\Kanji\nov-21.csv");
             using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-            _sourceSet = csv.GetRecords<Card>().ToArray();
+            _originalSet = csv.GetRecords<Card>().ToArray();
+            _sourceSet = _originalSet.ToList();
             _rnd.Shuffle(SourceSet);
         }
 
         [ICommand] private Task PrevCard() => Go(-1);
-        [ICommand] private Task NextCard() => Go(1);
+        [ICommand]
+        private Task CorrectGoNext()
+        {
+            return Go(1);
+        }
+
+        [ICommand]
+        private Task WrongTaskGoNext()
+        {
+            _sourceSet.Insert(CurrentTermIndex + 2, CurrentCard);
+            OnPropertyChanged(nameof(SourceSetLength));
+            _answerIsRead = true;
+            return Go(1);
+        }
 
         private async Task Go(int i)
         {
-            var nextVal = (CurrentTermIndex + i) % SourceSet.Length;
+            if (ReadAnswerEnabled && !_answerIsRead)
+            {
+                _synthesizer.Voice = SpeechSynthesizer.AllVoices.First(v => v.Language == "ja-JP");
+                var answer = await _synthesizer.SynthesizeTextToStreamAsync(CurrentCard.ToPronounce);
+                BackgroundMediaPlayer.Current.Source =
+                    MediaSource.CreateFromStream(answer, answer.ContentType);
+                _answerIsRead = true;
+                return;
+            }
+            _synthesizer.Voice = SpeechSynthesizer.DefaultVoice;
+            var nextVal = (CurrentTermIndex + i) % SourceSet.Count;
             if (nextVal == -1) return;
             if (nextVal == 0 && i == 1)
             {
+                _originalSet = SourceSet.ToArray();
                 _rnd.Shuffle(SourceSet);
+                OnPropertyChanged(nameof(SourceSetLength));
                 BackgroundMediaPlayer.Current.SetUriSource(
                     new Uri("ms-winsoundevent:Notification.SMS"));
                 await Task.Delay(TimeSpan.FromSeconds(1));
             }
             CurrentTermIndex = nextVal;
-            var stream = await _synthesizer.SynthesizeTextToStreamAsync(CurrentCard.ToPronounce);
+            var stream = await _synthesizer.SynthesizeTextToStreamAsync(CurrentCard.Meaning);
             BackgroundMediaPlayer.Current.Source =
                 MediaSource.CreateFromStream(stream, stream.ContentType);
+            _answerIsRead = false;
         }
-
-        [ObservableProperty, AlsoNotifyChangeFor(nameof(CurrentCard))]
-        private int _currentTermIndex;
-
-        [ObservableProperty, AlsoNotifyChangeFor(nameof(CurrentCard)), AlsoNotifyChangeFor(nameof(SourceSetLength))]
-        private Card[] _sourceSet;
-
-        public Card CurrentCard => _sourceSet[CurrentTermIndex];
-        public string SourceSetLength => SourceSet.Length.ToString();
-
     }
 }
