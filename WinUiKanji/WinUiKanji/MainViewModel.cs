d35fdb1de65ddf76b19402eab44b0e1ca96510cb
Ilya Margolin <imargolin@luware.com>
2021-11-07 19:30:00 +0300

    old ui

--- WinUiKanji/WinUiKanji/MainViewModel.cs
+++ WinUiKanji/WinUiKanji/MainViewModel.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Windows.Media.Core;
-using Windows.Media.Devices;
 using Windows.Media.Playback;
 using Windows.Media.SpeechSynthesis;
-using Windows.Media.Streaming.Adaptive;
+using CsvHelper;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
 
@@ -18,19 +17,20 @@ namespace WinUiKanji
         private readonly Random _rnd = new();
         private readonly SpeechSynthesizer _synthesizer = new()
         {
-            Voice = SpeechSynthesizer.AllVoices
-                .First(v => v.Language == "ja-JP")
+            Voice = SpeechSynthesizer.AllVoices.First(v => v.Language == "ja-JP")
         };
 
         public MainViewModel()
         {
             BackgroundMediaPlayer.Current.AutoPlay = true;
+            using var reader = new StreamReader(@"C:\git\Kanji\WordsToStudy4.csv");
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            _sourceSet = csv.GetRecords<Card>().ToArray();
         }
 
         [ICommand]
         private void MoveNext()
         {
-            if (SourceSet == null) return;
             var nextVal = (CurrentTermIndex + 1) % SourceSet.Length;
             if (nextVal == 0)
                 _rnd.Shuffle(SourceSet);
@@ -39,20 +39,25 @@ namespace WinUiKanji
         }
         private async Task Pronounce()
         {
-            var stream = await _synthesizer.SynthesizeTextToStreamAsync(
-                CurrentTerm.Kanji);
-            BackgroundMediaPlayer.Current.Source = MediaSource
-                .CreateFromStream(stream, stream.ContentType);
+            var stream = await _synthesizer.SynthesizeTextToStreamAsync(CurrentKanji);
+            BackgroundMediaPlayer.Current.Source = 
+                MediaSource.CreateFromStream(stream, stream.ContentType);
         }
 
         [ObservableProperty]
-        [AlsoNotifyChangeFor(nameof(CurrentTerm))]
-        private int _currentTermIndex = -1;
+        [AlsoNotifyChangeFor(nameof(CurrentKanji))]
+        [AlsoNotifyChangeFor(nameof(CurrentMeaning))]
+        [AlsoNotifyChangeFor(nameof(CurrentToPronounce))]
+        private int _currentTermIndex;
 
         [ObservableProperty]
-        [AlsoNotifyChangeFor(nameof(CurrentTerm))]
+        [AlsoNotifyChangeFor(nameof(CurrentKanji))]
+        [AlsoNotifyChangeFor(nameof(CurrentMeaning))]
+        [AlsoNotifyChangeFor(nameof(CurrentToPronounce))]
         private Card[] _sourceSet;
 
-        public Card CurrentTerm => _sourceSet[CurrentTermIndex];
+        public string CurrentKanji => _sourceSet[CurrentTermIndex].Kanji;
+        public string CurrentMeaning => _sourceSet[CurrentTermIndex].Meaning;
+        public string CurrentToPronounce => _sourceSet[CurrentTermIndex].ToPronounce;
     }
 }
